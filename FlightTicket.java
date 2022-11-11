03fb1db4e7872513df234a975a38826465b43c9e
sudeballioglu <84450861+sudeballioglu@users.noreply.github.com>
2022-11-11 18:32:21 +0300

    Update FlightTicket.java

--- FlightTicket.java
+++ FlightTicket.java
@@ -3,20 +3,7 @@ package Two;
 import java.util.Scanner;
 
 public class FlightTicket {
-	/*
-	 * mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın.
-	 * Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi (Tek Yön, Gidiş-Dönüş) bilgilerini alın.
-	 *  Mesafe başına ücret 0,10 TL / km olarak alın.
-	 *  İlk olarak uçuşun toplam fiyatını hesaplayın ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimleri uygulayın ;
-	 *  ! Kullanıcıdan alınan değerler geçerli(mesafe ve yaş değerleri pozitif sayı,yolculuk tipi ise 1 veya 2) olmalıdır.
-	 *    Aksi takdirde kullanıcıya "Hatalı Veri Girdiniz !" şeklinde bir uyarı verilmelidir.
-	 *  ! Kişi 12 yaşından küçükse bilet fiyatı üzerinden %50 indirim uygulanır.
-	 *  !Kişi 12-24 yaşları arasında ise bilet fiyatı üzerinden %10 indirim uygulanır.
-	 *  !Kişi 65 yaşından büyük ise bilet fiyatı üzerinden %30 indirim uygulanır.
-	 *  !Kişi "Yolculuk Tipini" gidiş dönüş seçmiş ise bilet fiyatı üzerinden %20 indirim uygulanır.
-	 *  
-	 */
-
+	
 	public static void main(String[] args) {
 		
 		int age,travelType,distance;
@@ -32,8 +19,6 @@ public class FlightTicket {
 		age=input.nextInt();
 		System.out.print("Yolculuk tipini giriniz (1=TEK YÖN, 2=GİDİŞ-DÖNÜŞ): ");
 		travelType=input.nextInt();
-		
-		
 		
 		if((distance<0)|| (age<0)) {
 			isError=true;
