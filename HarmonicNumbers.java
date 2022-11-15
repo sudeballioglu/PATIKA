2e444cd29a7696f36fd7b96b337b729cd437fff1
sudeballioglu <84450861+sudeballioglu@users.noreply.github.com>
2022-11-15 19:39:44 +0300

    Update HarmonicNumbers.java

--- HarmonicNumbers.java
+++ HarmonicNumbers.java
@@ -7,14 +7,13 @@ public class HarmonicNumbers {
 	public static void main(String[] args) {
 
 		
-		
 		Scanner input = new Scanner (System.in);
 		System.out.print("N sayısını giriniz: ");
 		int n =input.nextInt();
 		double result=0;
 		
 		for(int i =1; i<=n;i++) {
-			result+=(1.0/i); //double alabilmek için double yazdık
+			result+=(1.0/i); 
 		}
 
 		System.out.println(result);
