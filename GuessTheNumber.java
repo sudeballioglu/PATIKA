3db730b55e6346872418ef6dbbe94e390e08a76a
sudeballioglu <84450861+sudeballioglu@users.noreply.github.com>
2022-12-23 22:59:54 +0300

    Add files via upload

---
+++ GuessTheNumber.java
@@ -0,0 +1,66 @@
+package Eight;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class GuessTheNumber {
+
+	public static void main(String[] args) {
+
+		Random rnd = new Random();
+		int number=rnd.nextInt(100);
+//		int number=(int) (Math.random()*100);
+		
+		Scanner scan = new Scanner (System.in);
+		int right=0;
+		int selected;
+		int[] wrong =new int[5];
+		boolean isWin=false;
+		boolean isWrong=false;
+		
+		System.out.println(number);
+		while(right<5) {
+			System.out.print("Lütfen tahmininizi giriniz: ");
+			selected=scan.nextInt();
+			
+			if(selected<0 || selected>100) {
+				System.out.println("Lütfen 0-100 arasında bir sayı seçiniz.");
+				if(isWrong) {
+					right++;
+					System.out.println("Çok fazla hatalı giriş yaptınız.Kalan hak: " + (5-right));
+				}else {
+					isWrong=true;
+					System.out.println("Bir daha hatalı giriş yaparsanız hakkınızdan düşülecektir.");
+				}
+				continue;
+			}
+			
+			if(selected==number) {
+				System.out.println("Tebrikler, dopru tahmin! Tahmin ettiğiniz sayı: " + number);
+				isWin=true;
+				break;
+			}else {
+				System.out.println("Hatalı bir sayı girdiniz!");
+				if(selected>number) {
+					System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
+				}else {
+					System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
+				}
+				
+				wrong[right++]=selected;
+				System.out.println("Kalan hak: " + (5-right));
+				
+			}
+		}
+		
+		if(!isWin) {
+			System.out.println("Kaybettiniz!");
+			if(!isWrong) {
+				System.out.println("Tahminleriniz: " + Arrays.toString(wrong));
+			}
+		}
+	}
+
+}
